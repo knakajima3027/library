ece652f4e74bd1f1bc5e919cc3f95a576747f9a1
kNakajima3027 <kamihate1012@gmail.com>
2019-02-20 11:41:38 +0900

    素因数分解

---
+++ prime_factor.py
@@ -0,0 +1,12 @@
+#nを素因数分解したリストを返す
+def prime_factor(n):
+    i = 2
+    table = []
+    while i * i <= n:
+        while n % i == 0:
+            n //= i
+            table.append(i)
+        i += 1
+    if n > 1:
+        table.append(n)
+    return table

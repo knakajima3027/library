87b0f325eab2f76905a8dfee7e8e35dd02329779
kNakajima3027 <kamihate1012@gmail.com>
2019-02-20 15:40:36 +0900

    べき乗

--- pos.py
+++ pos.py
@@ -3,11 +3,9 @@
 m = 10**9 + 7
 
 def pos(x, n, m):
-    res = 1
-    while n > 0:
-        if n%2 == 1:
-            res = (res * x)%m
-        x = (x * x)%m
-        n = n // 2
-
+    if n == 0:
+        return 1
+    res = pos(x*x%m, n//2, m)
+    if n%2 == 1:
+        res = res*x%m
     return res

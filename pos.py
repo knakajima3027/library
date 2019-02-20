f065dbb602e07c0701a4fafa5ccdeb321dfe6c0f
kNakajima3027 <kamihate1012@gmail.com>
2019-02-20 15:08:19 +0900

    べき乗

--- pos.py
+++ pos.py
@@ -1,11 +1,13 @@
-#aのb乗をmで割った余り
+#xのn乗をmで割った余り
 
 m = 10**9 + 7
 
-def pos(a, b, m):
-    if b == 0:
-        return 1
-    res = (a * a)**(b // 2)%m
-    if b%2 == 1:
-        res = (res * a)%m
+def pos(x, n, m):
+    res = 1
+    while n > 0:
+        if n%2 == 1:
+            res = (res * x)%m
+        x = (x * x)%m
+        n = n // 2
+
     return res

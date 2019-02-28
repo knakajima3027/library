655bab44061add2d8499dc7f32c73e1907cf7c8c
kNakajima3027 <kamihate1012@gmail.com>
2019-02-28 14:13:56 +0900

    約数の列挙

---
+++ divisor.py
@@ -0,0 +1,11 @@
+def divisor(n):
+    i = 1
+    table = []
+    while i * i <= n:
+        if n%i == 0:
+            table.append(i)
+            table.append(n//i)
+        i += 1
+    
+    table.sort()
+    return table

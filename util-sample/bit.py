8edbc3395bc21e9d5154630a83502410526ae81f
kNakajima3027 <kamihate1012@gmail.com>
2020-08-31 22:22:58 +0900

    add bit_list

--- util-sample/bit.py
+++ util-sample/bit.py
@@ -4,3 +4,23 @@ import itertools
 def bit(n, m):
     bit_list = list(itertools.product([i for i in range(m)], repeat=n))
     return bit_list
+
+
+# 自作版
+def bit_list(x: int, n: int) -> list:
+    '''
+    xのn乗のbit配列を返す
+    '''
+    ans = []
+    for i in range(x ** n):
+        num = i
+        j = 0
+        table = [0 for _ in range(n)]
+        while num:
+            table[j] = num % x
+            num = num // x
+            j += 1
+
+        ans.append(table)
+
+    return ans

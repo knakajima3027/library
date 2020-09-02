5bff6ff985ae322b285b6464ab6c89658c338e8b
kNakajima3027 <kamihate1012@gmail.com>
2020-09-02 16:55:38 +0900

    型・テストを追加

--- algorithms/lcm.py
+++ algorithms/lcm.py
@@ -1,9 +1,14 @@
-#a,bの最大公約数
-def gcd(a, b):
-    while b:
-        a, b = b, a % b
-    return a
+#x,yの最大公約数 計算量: O(logn)
+def gcd(x: int, y: int) -> int:
+    while y:
+        x, y = y, x % y
+    return x
+
 
 #a,bの最小公倍数
-def lcm(a, b):
+def lcm(a: int, b: int) -> int:
     return a * b // gcd (a, b)
+
+
+assert lcm(6, 12) == 12
+assert lcm(67719, 17214) == 388571622

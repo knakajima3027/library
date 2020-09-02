dda6b2f54a7c9821972e78a7d4c4f003bb4908ea
kNakajima3027 <kamihate1012@gmail.com>
2020-09-02 16:56:54 +0900

    型を追加

--- algorithms/warshall_floyd.py
+++ algorithms/warshall_floyd.py
@@ -1,4 +1,6 @@
-def warshall_floyd(d, V): #d[i][j]は2頂点間i, j間の移動コストを格納, Vは頂点数
+# d[i][j]は2頂点間i, j間の移動コストを格納, Vは頂点数
+# 計算量: O(n^3)
+def warshall_floyd(d: list, V: int) -> list: 
     for k in range(V):
         for i in range(V):
             for j in range(V):
